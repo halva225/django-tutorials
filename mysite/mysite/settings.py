49a2805c074ac0e9ada3e70afd0d4b0e0d3d56fd
halva225 <zenasaulak@gmail.com>
2023-04-19 22:53:16 +0200

    added models

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'polls',
+        'USER': 'postgres',
+        'PASSWORD': 'm3C1j4C1',
+        'HOST': 'localhost'
     }
 }
 
