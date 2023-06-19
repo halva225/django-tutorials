ddca87c1bf97ec0255603753b718318c3f9e738f
Your Name <you@example.com>
2023-06-19 10:00:23 +0200

    adapted matrix initialization in search levenshtein algo

--- mysite/polls/algorithms/levenshtein_search.py
+++ mysite/polls/algorithms/levenshtein_search.py
@@ -1,3 +1,30 @@
+def levenshteinDistanceMatrix(s, t):
+    rows = len(s)+1
+    cols = len(t)+1
+    dist = [[0 for x in range(cols)] for x in range(rows)]
+
+    for i in range(1, rows):
+        dist[i][0] = i
+
+    for i in range(1, cols):
+        dist[0][i] = i
+        
+    for col in range(1, cols):
+        for row in range(1, rows):
+            if s[row-1] == t[col-1]:
+                cost = 0
+            else:
+                cost = 1
+            dist[row][col] = min(dist[row-1][col] + 1,      # deletion
+                                 dist[row][col-1] + 1,      # insertion
+                                 dist[row-1][col-1] + cost) # substitution
+    
+    
+ 
+    return dist[row][col]
+
+
+
 def calcDictDistance(word, numWords):
     file = open('mysite/polls/algorithms/1-1000.txt', 'r') 
     lines = file.readlines() 
@@ -16,7 +43,7 @@ def calcDictDistance(word, numWords):
     wordDetails = []
     currWordDist = 0
     dictWordDist.sort()
-    print(dictWordDist)
+    
     for i in range(numWords):
         currWordDist = dictWordDist[i]
         wordDetails = currWordDist.split("-")
